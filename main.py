7960ab986aa3472b64dd3fd095bdacd82cf7de0f
mlo_bootdev <rainbow_5@web.de>
2025-06-07 16:59:41 +0200

    refactored main

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from stats import sort_dict
 
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     book_path = sys.argv[1]
+    
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_character_count(text)
@@ -28,9 +32,5 @@ def get_book_text(path):
         return f.read()
 
 
-if len(sys.argv) < 2:
-    print("Usage: python3 main.py <path_to_book>")
-    sys.exit(1)
-else:
 
-    main()
+main()
